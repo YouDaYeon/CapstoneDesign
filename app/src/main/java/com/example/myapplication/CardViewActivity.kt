d53794c5bc9761cb8af9e2977b6c85420b8798d1
Dayeon <dayean1@naver.com>
2023-11-04 10:20:35 +0900

    명합 팝업

--- app/src/main/java/com/example/myapplication/CardViewActivity.kt
+++ app/src/main/java/com/example/myapplication/CardViewActivity.kt
@@ -3,7 +3,6 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
 
 class CardViewActivity : AppCompatActivity() {
 
@@ -14,19 +13,11 @@ class CardViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_view)
 
+
         //넘어온 데이터 변수에 담기
         Name = intent.getStringExtra("name").toString()
         Uid = intent.getStringExtra("uId").toString()
 
-        // 툴바 설정
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val titleText = "$Name 님의 명함"
-
-        // 툴바의 TextView를 찾아서 텍스트 설정
-        val titleTextView: TextView = toolbar.findViewById(R.id.name)
-        titleTextView.text = titleText
 
         // 명함에 이름 설정
         val userName: TextView = findViewById(R.id.studentname)
