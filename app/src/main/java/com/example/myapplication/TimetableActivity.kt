a033afc45de083824d88666809f48b8d22295829
김찬영 <ehwjr405@naver.com>
2023-11-05 19:36:02 +0900

    chan

---
+++ app/src/main/java/com/example/myapplication/TimetableActivity.kt
@@ -0,0 +1,66 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class TimetableActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+    private val timeSlots = listOf(
+        "9_10", "10_11", "11_12", "12_13", "13_14",
+        "14_15", "15_16", "16_17", "17_18", "18_19"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timetable)
+
+        sharedPreferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+
+        for (day in listOf("monday", "tuesday", "wednesday", "thursday", "friday")) {
+            for (slot in timeSlots) {
+                val editTextId = resources.getIdentifier("${day}_${slot}", "id", packageName)
+                val editText = findViewById<EditText>(editTextId)
+                editText.setText(sharedPreferences.getString("${day}_${slot}", ""))
+            }
+        }
+
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        saveButton.setOnClickListener {
+            val editor = sharedPreferences.edit()
+
+            for (day in listOf("monday", "tuesday", "wednesday", "thursday", "friday")) {
+                for (slot in timeSlots) {
+                    val editTextId = resources.getIdentifier("${day}_${slot}", "id", packageName)
+                    val editText = findViewById<EditText>(editTextId)
+                    val key = "${day}_${slot}"
+                    editor.putString(key, editText.text.toString())
+                    editText.text.clear()
+                }
+            }
+
+            editor.apply()
+            Toast.makeText(this, "시간표가 저장되었습니다!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val editor = sharedPreferences.edit()
+
+        for (day in listOf("monday", "tuesday", "wednesday", "thursday", "friday")) {
+            for (slot in timeSlots) {
+                val editTextId = resources.getIdentifier("${day}_${slot}", "id", packageName)
+                val editText = findViewById<EditText>(editTextId)
+                val key = "${day}_${slot}"
+                editor.putString(key, editText.text.toString())
+            }
+        }
+
+        editor.apply()
+    }
+}
